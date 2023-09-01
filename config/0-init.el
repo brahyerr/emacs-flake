879b74fa565c4af86d47d5ab4fc39440432c7192
brahyerr <bp1829847@gmail.com>
2023-09-01 02:06:42 -0400

    change default font

--- config/0-init.el
+++ config/0-init.el
@@ -1,8 +1,10 @@
-; test
+:; test
 (setq inhibit-startup-message nil
       visible-bell nil)
 (setq ring-bell-function 'ignore)
 (load-theme 'modus-vivendi t)
+
+(set-face-attribute 'default nil :font "lemonscaled-20")
 
 ; (require 'lsp-mode)
 (add-hook 'css-mode-hook #'lsp)
