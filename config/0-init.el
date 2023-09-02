eb047ba4e04d358e1b8d0bdbcc7cefc2fd884fe4
brahyerr <bp1829847@gmail.com>
2023-09-02 03:39:55 -0400

    add some packages and config options

--- config/0-init.el
+++ config/0-init.el
@@ -2,9 +2,15 @@
 (setq inhibit-startup-message nil
       visible-bell nil)
 (setq ring-bell-function 'ignore)
-(load-theme 'modus-vivendi t)
+(load-theme 'wombat t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(menu-bar-mode -1)
 
-(set-face-attribute 'default nil :font "lemonscaled-20")
+(set-face-attribute 'default nil :font "lemonscaled" :height 200)
+(set-fontset-font t nil (font-spec :size 20 :name "CozetteHiDpi"))
 
 ; (require 'lsp-mode)
 (add-hook 'css-mode-hook #'lsp)
--- config/packages.el
+++ config/packages.el
@@ -1,7 +1,58 @@
 ;; (add-to-list 'load-path (expand-file-name "./" (file-name-directory load-file-name)))
 ;; (load "init.el")
 
-(use-package meow
-             :ensure t)
-(use-package magit
-             :ensure t)
+(use-package meow :ensure t)
+(use-package magit :ensure t)
+(use-package command-log-mode :ensure t)
+(use-package linum-relative :ensure t)
+
+;;; Languages
+
+;; Nix
+(use-package nix-mode
+  :ensure t  
+  :mode "\\.nix\\'")
+
+;; Python
+(use-package python
+  :ensure t
+  :mode ("\\.py\\'" . python-mode))
+
+;; Formatting
+; (setq-default indent-tabs-mode nil)
+; (setq-default tab-width 2)
+; (defvaralias 'c-basic-offset 'tab-width)
+; (defvaralias 'cperl-indent-level 'tab-width)
+; (setq-default sh-basic-offset 2)
+; (setq-default js-indent-level 2)
+; (setq-default css-indent-offset 2)
+; (setq-default web-mode-markup-indent-offset 2)
+; (setq-default web-mode-css-indent-offset 2)
+; (setq-default web-mode-code-indent-offset 2)
+; (setq-default typescript-indent-level 2)
+
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1))
+
+; (use-package all-the-icons
+;  :if (display-graphic-p))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-ir-black t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  ; (doom-themes-neotree-config)
+  ;; or for treemacs users
+  ; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  ; (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
