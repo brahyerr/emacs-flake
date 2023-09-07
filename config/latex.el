3bc6b51b3eff7c80bda2a668f6b2847c96688d43
brahyerr <bp1829847@gmail.com>
2023-09-07 15:17:56 -0400

    add latex config

--- config/latex.el
+++ config/latex.el
@@ -1,5 +1,5 @@
-;; (setq-default TeX-PDF-mode t)   ; Use PDF mode by default
-;; (setq TeX-source-correlate-mode t) ; Enable source correlation
-;; (setq TeX-source-correlate-method 'synctex) ; Set correlation method
-;; (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-;; (setq TeX-view-program-selection '((output-pdf "zathura")))
+(setq-default TeX-PDF-mode t)   ; Use PDF mode by default
+(setq TeX-source-correlate-mode t) ; Enable source correlation
+(setq TeX-source-correlate-method 'synctex) ; Set correlation method
+(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+(setq TeX-view-program-selection '((output-pdf "zathura")))
