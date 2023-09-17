6d8a0b8bc0044f3c98c89c44b3e9bce28b14468d
brahyerr <bp1829847@gmail.com>
2023-09-17 15:33:52 -0400

    refactor flake, split epkgs declaration, and update readme to reflect new changes

---
+++ epkgs.nix
@@ -0,0 +1,5 @@
+epkgs: with epkgs; [
+  ### Add your emacs packages here ###
+  # lsp-mode
+  # treesit-grammars.with-all-grammars
+]
--- flake.nix
+++ flake.nix
@@ -5,35 +5,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
+    
+  outputs = {
+    self,
+    nixpkgs,
+    emacs-overlay,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    withSystem = f:
+      lib.fold lib.recursiveUpdate {}
+      (map f ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]);
+  in
+    withSystem (system: let
+      pkgs = import nixpkgs { inherit system; overlays = [ emacs-overlay.overlays.default ]; };
+      emacs-unwrapped = pkgs.emacsWithPackagesFromUsePackage {
 
-  outputs = { self, nixpkgs, emacs-overlay, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; overlays = [ emacs-overlay.overlays.default ]; };
-    emacs-unwrapped = pkgs.emacsWithPackagesFromUsePackage {
-      # Create an init file by concatenating the the contents of the files in config/*.el
-      defaultInitFile = true;
-      config = pkgs.writeTextFile {
-        name = "emacs-config.el";
-        text = (builtins.readFile ./config/init.el) 
-             + (builtins.readFile ./config/packages.el);
+        defaultInitFile = true;
+        config = pkgs.writeTextFile {
+          name = "emacs-config.el";
+          text = (builtins.readFile ./config/init.el) 
+               + (builtins.readFile ./config/packages.el);
+        };
+        package = if (system == "x86_64-linux" || system == "aarch64-linux")
+                  then pkgs.emacs-pgtk        # Emacs with experimental gtk wayland support
+                  else pkgs.emacs29-macport;
+        extraEmacsPackages = import ./epkgs.nix;
+        alwaysEnsure = true;
       };
-      package = pkgs.emacs-unstable;
-      extraEmacsPackages = epkgs: [
-        epkgs.ripgrep
-        epkgs.fzf
-        epkgs.lsp-mode
-        epkgs.lsp-ui
-      ];
-      # package = pkgs.emacs-unstable.overrideAttrs (final: prev: {
-      #   nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-      #   postInstall = prev.postInstall + ''
-      #     wrapProgram $out/bin/emacs \
-      #       --suffix PATH : ${with pkgs; lib.makeBinPath [ fzf ripgrep ]}
-      #   '';
-      # });
-      # Sets config to be used as default init file
-    };
+      
       # Wrap the emacs binary with extra external programs to be available in its PATH
       emacs-wrapped = with pkgs; symlinkJoin {
         name = "emacs";
@@ -41,20 +41,20 @@
         paths = [ emacs-unwrapped ];
         postBuild = let 
           path = [
-            fzf
-            ripgrep
-            fd
+            ### Add your external programs here ###
+            # fzf
+            # ripgrep
           ];
         in ''
-          wrapProgram $out/bin/emacs \
-            --prefix PATH : ${lib.makeBinPath path}
+        wrapProgram $out/bin/emacs \
+          --prefix PATH : ${lib.makeBinPath path}
         '';
       };
-  in
-  {
-    packages.${system}.default = emacs-wrapped;
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [ emacs-wrapped ];
-    };
-  };
+    in {
+      formatter.${system} = pkgs.alejandra;
+      packages.${system}.default = emacs-wrapped;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ emacs-wrapped ];
+      };
+    });
 }
