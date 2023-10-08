5250c064df38bbe4e63a0ac2130a227c6ee147cd
brahyerr <bp1829847@gmail.com>
2023-10-08 00:29:20 -0400

    edit font config

--- config/fonts.el
+++ config/fonts.el
@@ -1,14 +1,14 @@
 ;; Set fonts for emacs
-(set-face-attribute 'default nil :font "lemonscaled" :height 200)
-(set-face-attribute 'tooltip nil :font "lemonscaled" :height 200)
+(set-face-attribute 'default nil :font "lemonHiDpi" :height 200)
+(set-face-attribute 'tooltip nil :font "lemonHiDpi" :height 200)
 
-(add-to-list 'default-frame-alist '(font . "lemonscaled-20"))
-(set-fontset-font t 'unicode (font-spec :name "lemonscaled") nil)
+(add-to-list 'default-frame-alist '(font . "lemonHiDpi"))
+(set-fontset-font t 'unicode (font-spec :name "lemonHiDpi") nil)
 ;; (set-fontset-font t 'unicode (font-spec :name "Noto Color Emoji") nil 'append)
 (set-fontset-font t 'unicode (font-spec :name "CozetteHiDpi") nil 'append)
 (set-fontset-font t 'unicode (font-spec :name "JetBrainsMono") nil 'append)
 (set-fontset-font t 'unicode (font-spec :name "DejaVu Sans Mono") nil 'append)
-(set-frame-font "lemonscaled" nil t)
+(set-frame-font "lemonHiDpi" nil t)
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Terminus" :height 160 :weight 'regular)
 ;; CJK fonts
@@ -20,22 +20,22 @@
 (set-fontset-font t 'hangul (font-spec :name "WenQuanYi Bitmap Song") nil)
 
 
-(defun set-fonts-for-emacsclient ()
-  (add-to-list 'default-frame-alist '(font . "lemonscaled-20"))
-  (set-fontset-font t 'unicode (font-spec :name "lemonscaled") nil)
-  (set-fontset-font t 'unicode (font-spec :name "Noto Color Emoji") nil 'append)
-  (set-fontset-font t 'unicode (font-spec :name "CozetteHiDpi") nil 'append)
-  (set-fontset-font t 'unicode (font-spec :name "JetBrainsMono") nil 'append)
-  (set-fontset-font t 'unicode (font-spec :name "DejaVu Sans Mono") nil 'append)
-  (set-frame-font "lemonscaled" nil t)
-  ;; Set the variable pitch face
-  (set-face-attribute 'variable-pitch nil :font "Terminus" :height 160 :weight 'regular)
-  ;; CJK fonts
-  (set-fontset-font t 'unicode (font-spec :name "WenQuanYi Bitmap Song") nil 'append)
-  (set-fontset-font t 'unicode (font-spec :name "WenQuanYi Bitmap Song") nil 'append)
-  (set-fontset-font t 'unicode (font-spec :name "WenQuanYi Bitmap Song") nil 'append)
-  (set-fontset-font t 'han (font-spec :name "WenQuanYi Bitmap Song") nil)
-  (set-fontset-font t 'kana (font-spec :name "WenQuanYi Bitmap Song") nil)
-  (set-fontset-font t 'hangul (font-spec :name "WenQuanYi Bitmap Song") nil))
+;; (defun set-fonts-for-emacsclient ()
+;;   (add-to-list 'default-frame-alist '(font . "lemonscaled-20"))
+;;   (set-fontset-font t 'unicode (font-spec :name "lemonscaled") nil)
+;;   (set-fontset-font t 'unicode (font-spec :name "Noto Color Emoji") nil 'append)
+;;   (set-fontset-font t 'unicode (font-spec :name "CozetteHiDpi") nil 'append)
+;;   (set-fontset-font t 'unicode (font-spec :name "JetBrainsMono") nil 'append)
+;;   (set-fontset-font t 'unicode (font-spec :name "DejaVu Sans Mono") nil 'append)
+;;   (set-frame-font "lemonscaled" nil t)
+;;   ;; Set the variable pitch face
+;;   (set-face-attribute 'variable-pitch nil :font "Terminus" :height 160 :weight 'regular)
+;;   ;; CJK fonts
+;;   (set-fontset-font t 'unicode (font-spec :name "WenQuanYi Bitmap Song") nil 'append)
+;;   (set-fontset-font t 'unicode (font-spec :name "WenQuanYi Bitmap Song") nil 'append)
+;;   (set-fontset-font t 'unicode (font-spec :name "WenQuanYi Bitmap Song") nil 'append)
+;;   (set-fontset-font t 'han (font-spec :name "WenQuanYi Bitmap Song") nil)
+;;   (set-fontset-font t 'kana (font-spec :name "WenQuanYi Bitmap Song") nil)
+;;   (set-fontset-font t 'hangul (font-spec :name "WenQuanYi Bitmap Song") nil))
 
-(add-hook 'server-after-make-frame-hook 'set-fonts-for-emacsclient)
+;; (add-hook 'server-after-make-frame-hook 'set-fonts-for-emacsclient)
