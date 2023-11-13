21d0480fe365bd48838510c621ac2fb640f01ebc
brahyerr <bp1829847@gmail.com>
2023-11-13 00:35:57 -0500

    * config/org-mode.el: change org-mode column width

--- config/org-mode.el
+++ config/org-mode.el
@@ -1,6 +1,6 @@
 ;; Pad and .org doc with whitespace
 (defun local/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
+  (setq visual-fill-column-width 200
 	visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
